0c9da7d1e15ef30edc7ef219c2e00bb074fad6f9
skylerspaeth <skylerspaeth.scc@me.com>
2020-12-06 16:53:25 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = require('./token.json').token;
+const utterances = require('./utterances.json').utterances;
+
+function getWord() {
+	return utterances[Math.floor(Math.random() * utterances.length)].toUpperCase() + " ";
+}
+
+function isAlpha(str) {
+  var code, i, len;
+
+  for (i = 0, len = str.length; i < len; i++) {
+    code = str.charCodeAt(i);
+    if (!(code > 64 && code < 91) && !(code > 96 && code < 123)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+function learify(text) {
+	if (text.includes(" ")) {
+		let ret = "";
+		text.split(" ").forEach((word) => {
+			if (isAlpha(word)) ret += getWord();
+			else ret += word + " ";
+		});
+		return ret;
+	} else {
+		return "LEARSENSE";
+	}
+}
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+	let guild = msg.guild;
+	if ((msg.channel.name === 'lear' || msg.channel.name === 'sense') && msg.author.id !== client.user.id) {
+		msg.channel.send(`${guild.member(msg.author) ? guild.member(msg.author).displayName : msg.author.username} said: "${learify(msg.content)}"`);
+	}
+});
+
+client.login(token);
